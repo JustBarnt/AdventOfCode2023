51ac60fbb7740c90525713b9b92b7f7a618d0547
Brent Williams <85238383+JustBarnt@users.noreply.github.com>
2023-12-22 14:41:56 -0500

    Trying to get CMAKE setup

---
+++ DayOne/DayOne.cpp
@@ -0,0 +1,13 @@
+// Headers
+// #include <fstream>
+// #include <string>
+#include <iostream>
+
+using namespace std;
+
+//Code
+int main()
+{
+    cout << "Hello World!\n" << endl;
+    return 0;
+}
